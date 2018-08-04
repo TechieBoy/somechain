c987c8272aaf3399e7775e88af0e5852a9f2ec26
Sanket Shanbhag <TechieBoy@users.noreply.github.com>
2018-08-04 13:07:38 +0530

    Added wallet creation

--- src/client.py
+++ src/client.py
@@ -1,6 +1,46 @@
 """
 TODO:
     Functions for interacting with api's
-    Create wallets
     Send and receive coins
 """
+
+import secrets
+import hashlib
+from typing import Tuple
+from utils.secp256k1 import point_mul, b58_encode
+
+Point = Tuple[int, int]
+
+
+class Wallet:
+    def __init__(self):
+        self.private_key = self.create_private_key()
+
+    @staticmethod
+    def create_private_key() -> bytearray:
+        return secrets.token_bytes(32)
+
+    def generate_address(self):
+        q = point_mul(int.from_bytes(self.private_key, byteorder='big'))
+        q_bytes = b"\x04" + q[0].to_bytes(32, byteorder='big') + q[1].to_bytes(32, byteorder='big')
+        hsh = hashlib.sha256(q_bytes).digest()
+        ripemd160 = hashlib.new('ripemd160')
+        ripemd160.update(hsh)
+        ripemd160 = ripemd160.digest()
+        address = b"\x00" + ripemd160
+        checksum = hashlib.sha256(hashlib.sha256(address).digest()).digest()[:4]
+        address += checksum
+
+        wif = b"\x80" + self.private_key
+        checksum = hashlib.sha256(hashlib.sha256(wif).digest()).digest()[:4]
+        wif += checksum
+
+        address = b58_encode(address)
+        wif = b58_encode(wif)
+
+        return address, wif
+
+
+if __name__ == "__main__":
+    w = Wallet()
+    print(w.generate_address())
---
+++ src/utils/secp256k1.py
@@ -0,0 +1,68 @@
+from typing import Tuple
+
+"""
+TODO:
+    add docstrings
+"""
+Point = Tuple[int, int]
+
+# https://en.bitcoin.it/wiki/Secp256k1
+P: int = (2 ** 256) - (2 ** 32) - (2 ** 9) - (2 ** 8) - (2 ** 7) - (2 ** 6) - (2 ** 4) - 1
+G: Point = (0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+            0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8)
+B58_char = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+
+def point_add(p: Point, q: Point) -> Point:
+    px, py = p
+    qx, qy = q
+
+    if p == q:
+        lmbda = pow(2 * py % P, P - 2, P) * (3 * px * px) % P
+    else:
+        lmbda = pow(qx - px, P - 2, P) * (qy - py) % P
+
+    rx = (lmbda ** 2 - px - qx) % P
+    ry = (lmbda * px - lmbda * rx - py) % P
+
+    return rx, ry
+
+
+def point_mul(d: int) -> Point:
+    p = G
+    n = p
+    q = None
+
+    for i in range(256):
+        if d & (1 << i):
+            if q is None:
+                q = n
+            else:
+                q = point_add(q, n)
+
+        n = point_add(n, n)
+
+    return q
+
+
+def b58_encode(d):
+    out = ""
+    p = 0
+    x = 0
+
+    while d[0] == 0:
+        out += "1"
+        d = d[1:]
+
+    for i, v in enumerate(d[::-1]):
+        x += v * (256 ** i)
+
+    while x > 58 ** (p + 1):
+        p += 1
+
+    while p >= 0:
+        a, x = divmod(x, 58 ** p)
+        out += B58_char[a]
+        p -= 1
+
+    return out
